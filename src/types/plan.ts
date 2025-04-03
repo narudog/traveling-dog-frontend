1f5d32d3010c79a7dcb32d769d3cfc2b6ef92d30
Naru <kwonnaru@kakao.com>
2025-04-03 11:34:20 +0900

    여행 계획 인터페이스 수정 및 새로운 속성 추가
    - TravelPlan 인터페이스에 숙박, 예산, 관심사, 일정, 교통수단, 여행 스타일 등의 새로운 속성을 추가하여 데이터 구조를 확장
    - Itinerary 및 Location 인터페이스를 새로 정의하여 여행 일정 및 위치 정보를 보다 명확하게 관리할 수 있도록 개선
    - 기존 속성의 순서를 조정하고, 일부 속성의 타입을 수정하여 코드의 일관성을 높임

--- src/types/plan.ts
+++ src/types/plan.ts
@@ -1,16 +1,22 @@
 export interface TravelPlan {
-  id: number;
-  title: string;
-  country: string;
+  accommodation: [{ id: number; name: string }];
+  budget: string;
   city: string;
-  startDate: string;
+  country: string;
   endDate: string;
-  userId: number;
-  nickname: string;
-  travelLocations: TravelLocation[];
-  viewCount: number;
+  id: number;
+  interests: [{ id: number; name: string }];
+  itineraries: Itinerary[];
   likeCount: number;
-  status: PlanStatus;
+  nickname: string | null;
+  season: string;
+  startDate: string;
+  status: string;
+  title: string;
+  transportation: [{ id: number; name: string }];
+  travelStyles: [{ id: number; name: string }];
+  userId: number | null;
+  viewCount: number;
   createdAt: string;
   updatedAt: string;
 }
@@ -29,15 +35,21 @@ export interface TravelPlanCreateRequest {
   transportation: string;
 }
 
-export interface TravelLocation {
+export interface Itinerary {
   id: number;
-  placeName: string;
-  longitude: number;
-  latitude: number;
+  date: number;
+  activities: Location[];
+  location: string;
+  lunch: Location;
+  dinner: Location;
+}
+
+export interface Location {
+  id: number;
+  name: string;
   description: string;
-  locationOrder: number;
-  createdAt: string;
-  updatedAt: string;
+  latitude: number;
+  longitude: number;
 }
 
 export enum PlanStatus {
