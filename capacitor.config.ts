e1d2b15ae9a23cbd3d99a7d058ec2b187fb590d8
Naru <kwonnaru@kakao.com>
2025-04-07 13:37:46 +0900

    앱 이름 및 아이콘 변경, 스플래시 화면 설정 추가
    - capacitor.config.ts 파일에서 앱 ID 및 이름을 'TravelingDog'로 변경하고 스플래시 화면 설정 추가
    - ionic.config.json 파일에서 앱 이름을 'TravelingDog'로 변경
    - 새로운 아이콘 및 스플래시 이미지 파일 추가
    - manifest.webmanifest 파일 생성하여 아이콘 설정 추가
    - .gitignore 파일에 Android 리소스 경로 추가

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,23 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.frontend",
-  appName: "frontend",
+  appId: "com.narudog.travelingdog",
+  appName: "TravelingDog",
   webDir: "public",
   server: {
     url: "https://traveling-dev.narudog.com", // 네 컴퓨터의 IP 주소
     cleartext: true, // HTTP 허용
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#000000",
+      androidSplashResourceName: "splash",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
   },
 };
 
