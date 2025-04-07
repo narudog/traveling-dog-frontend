d679490791c4357449d15853802119e6017247da
Naru <kwonnaru@kakao.com>
2025-04-07 12:37:07 +0900

    Capacitor 설정 업데이트 및 패키지 의존성 추가
    - capacitor.config.ts 파일에서 웹 디렉토리를 'build'에서 'public'으로 변경하고 서버 설정 추가
    - package.json에 '@capacitor/assets' 의존성 추가

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.frontend',
-  appName: 'frontend',
-  webDir: 'build'
+  appId: "com.frontend",
+  appName: "frontend",
+  webDir: "public",
+  server: {
+    url: "https://traveling-dev.narudog.com", // 네 컴퓨터의 IP 주소
+    cleartext: true, // HTTP 허용
+  },
 };
 
 export default config;
