701c7249117a43da3b20f2abf2e3f77c675090e9
Naru <kwonnaru@kakao.com>
2025-04-13 13:38:27 +0900

    'UnauthenticatedButtons' 헤더에 '내 계획 '버튼 추가

--- src/components/commons/UnauthenticatedButtons.tsx
+++ src/components/commons/UnauthenticatedButtons.tsx
@@ -2,22 +2,19 @@ import Link from "next/link";
 import styles from "./Header.module.scss";
 
 const UnauthenticatedButtons = () => {
-  return (
-    <>
-      <Link
-        href="/login"
-        className={`${styles.headerButton} ${styles.loginButton}`}
-      >
-        로그인
-      </Link>
-      <Link
-        href="/signup"
-        className={`${styles.headerButton} ${styles.signupButton}`}
-      >
-        회원가입
-      </Link>
-    </>
-  );
+    return (
+        <>
+            <Link href="/travel-plan/list" className={`${styles.headerButton} ${styles.travelPlanButton}`}>
+                내 여행
+            </Link>
+            <Link href="/login" className={`${styles.headerButton} ${styles.loginButton}`}>
+                로그인
+            </Link>
+            <Link href="/signup" className={`${styles.headerButton} ${styles.signupButton}`}>
+                회원가입
+            </Link>
+        </>
+    );
 };
 
 export default UnauthenticatedButtons;
