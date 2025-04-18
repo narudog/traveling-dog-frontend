3cf84d6bbc9be3f4b898d5d54255617dc5eca882
Naru <kwonnaru@kakao.com>
2025-04-18 11:53:55 +0900

    로딩 상태에 따라 인증 버튼 표시 방식 변경 및 코드 정리

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -8,33 +8,33 @@ import { useAuthStore } from "@/store/auth";
 import { useEffect } from "react";
 import UnauthenticatedButtons from "./UnauthenticatedButtons";
 const Header = () => {
-    const { user, getUserProfile } = useAuthStore();
+  const { user, getUserProfile, loading } = useAuthStore();
 
-    useEffect(() => {
-        if (!user) {
-            getUserProfile();
-        }
-    }, []);
+  useEffect(() => {
+    if (!user) {
+      getUserProfile();
+    }
+  }, []);
 
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <Link href="/">
-                    <Image src="/logo.webp" alt="Logo" width={120} height={40} />
-                </Link>
-            </div>
-            <div className={styles.authButtons}>
-                {!user ? (
-                    <div className={styles.skeleton}>
-                        <div className={styles.skeletonButton}></div>
-                        <div className={styles.skeletonButton}></div>
-                    </div>
-                ) : (
-                    <>{user.id ? <AuthenticatedButtons /> : <UnauthenticatedButtons />}</>
-                )}
-            </div>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <Image src="/logo.webp" alt="Logo" width={120} height={40} />
+        </Link>
+      </div>
+      <div className={styles.authButtons}>
+        {loading ? (
+          <div className={styles.skeleton}>
+            <div className={styles.skeletonButton}></div>
+            <div className={styles.skeletonButton}></div>
+          </div>
+        ) : (
+          <>{user ? <AuthenticatedButtons /> : <UnauthenticatedButtons />}</>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
